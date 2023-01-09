21506c0552463a22871a4dfd8ecc5c82ec13cfbc
Apuromafo <astronado@gmail.com>
2023-01-09 10:37:16 -0300

    Update script1.py
    se actualizan comentarios de # a """

--- script1.py
+++ script1.py
@@ -1,14 +1,14 @@
-#source : https://t.me/aprenderpython 
-#Primer reto Nivel Facil:
-# Crea un programa que lea la hora de tu pc y que cuando ejecutes el programa, te salude dependiendo la hora, ej
-#"Buen dia", "Buenas tardes" o "Buenas noches".
-#primero instalamos la librería datetime
-#pip install datetime
-#ahora configuramos a manejar el saludo según la hora del día del pc.
-#Mañana: del amanecer al mediodía (00:00 – 12:00)
-#Tarde: del mediodía al atardecer (12:00 – 18.00)
-# Noche: del atardecer a la medianoche (18.00 – 24:00)
-
+"""source : https://t.me/aprenderpython 
+Primer reto Nivel Facil:
+ Crea un programa que lea la hora de tu pc y que cuando ejecutes el programa, te salude dependiendo la hora, ej
+"Buen dia", "Buenas tardes" o "Buenas noches".
+primero instalamos la librería datetime
+pip install datetime
+ahora configuramos a manejar el saludo según la hora del día del pc.
+Mañana: del amanecer al mediodía (00:00 – 12:00)
+Tarde: del mediodía al atardecer (12:00 – 18.00)
+ Noche: del atardecer a la medianoche (18.00 – 24:00)
+"""
 import datetime as d
 
 HH = d.datetime.now()
@@ -24,7 +24,7 @@ elif (H>=12 and m<59) and (h<17   and m<=59):
 else:
     print("Buenas Noches!!")
 
-#ejemplo de salida de consola
-#Son las :   10 : 23
-#Buenos días!!
-
+"""ejemplo de salida de consola
+Son las :   10 : 23
+Buenos días!!
+"""
