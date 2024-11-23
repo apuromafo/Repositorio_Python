d4a1ad374ef3c70994aa23dbf4f0b1b36ebc17df
Apuromafo <astronado@gmail.com>
2024-11-23 20:15:07 -0300

    Update

--- 012_B64/Readme.md.md
+++ 012_B64/Readme.md.md
@@ -1,3 +1,9 @@
 Pequeña tool para abordar archivos de base64 si, se tilda permite decodificar.
+al ser interfaz gráfica, requiere python3 b64.py
 
-![[Pasted image 20241118193752.png]]
+
+![](./img/img1.png)
+
+
+
+personalmente recomiendo más el uso de cyberchef, pero nunca está de más intentar armar algo.
---
+++ 014_Banner_Art/poc_uso_banner/002_only_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def print_banner():
+    banner = r"""
+.d8888.  .o88b. d8888b. d888888b d8888b. d888888b 
+88'  YP d8P  Y8 88  `8D   `88'   88  `8D `~~88~~' 
+`8bo.   8P      88oobY'    88    88oodD'    88    
+  `Y8b. 8b      88`8b      88    88~~~      88    
+db   8D Y8b  d8 88 `88.   .88.   88         88    
+`8888Y'  `Y88P' 88   YD Y888888P 88         YP    
+                     v0.1 
+"""
+    print (banner)
+def main():
+    print_banner()
+     
+if __name__ == "__main__":
+    main()    
