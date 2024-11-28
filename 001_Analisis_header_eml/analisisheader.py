49ec300751902b10be845cfbb9417490ba6e1122
Apuromafo <astronado@gmail.com>
2024-11-28 11:51:09 -0300

    Update analisisheader.py

--- 001_Analisis_header_eml/analisisheader.py
+++ 001_Analisis_header_eml/analisisheader.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-description = 'Herramienta para análisis de Header '
+description = 'Herramienta para análisis de Header EML'
 author = 'Apuromafo'
 version = '0.0.3'
 date = '28.11.2024'
