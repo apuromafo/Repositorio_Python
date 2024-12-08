b118c1838cf53b50e6a8092628e2b36092ff12dc
Apuromafo <astronado@gmail.com>
2024-12-08 01:56:24 -0300

    Update sha256.py

--- 21_sha256/Gen/sha256.py
+++ 21_sha256/Gen/sha256.py
@@ -1,6 +1,48 @@
+#!/usr/bin/env python
+
+description = 'pequeño generador de sha-256 en minúscula y mayúscula'
+author = 'Apuromafo'
+version = '0.0.1'
+date = '08.12.2024'
+
 import hashlib
 import argparse
+import sys
+import random
+import time  # sleep
 
+def print_banner():
+    clear = "\x1b[0m"  # color reset
+
+    # Mapa de colores
+    colors = {
+        "magenta": "\x1b[35m",
+        "red": "\x1b[31m",
+        "green": "\x1b[32m",
+        "yellow": "\x1b[33m",
+        "blue": "\x1b[34m",
+        "cyan": "\x1b[36m",
+        "white": "\x1b[37m"
+    }
+
+    banner = r"""
+         __         ___   ___________
+   _____/ /_  ____ |__ \ / ____/ ___/
+  / ___/ __ \/ __ `/_/ //___ \/ __ \
+ (__  ) / / / /_/ / __/____/ / /_/ /
+/____/_/ /_/\__,_/____/_____/\____/
+  
+                     v0.1 
+"""
+
+    # Elegir colores aleatorios de las claves del diccionario
+    color_keys = list(colors.keys())
+    
+    for line in banner.split("\n"):
+        color = random.choice(color_keys)  # Elegir un color aleatorio
+        sys.stdout.write(f"{colors[color]}{line}{clear}\n")  # Imprimir con color del mapa
+        time.sleep(0.03)  # Pausa para efecto de tipo máquina de escribir
+        
 def calcular_sha256(texto):
   """Calcula el hash SHA-256 de un texto dado.
 
@@ -22,11 +64,18 @@ def calcular_sha256(texto):
   hash_hex = sha256.hexdigest()
 
   return hash_hex
-
-if __name__ == "__main__":
+  
+def main():
   parser = argparse.ArgumentParser(description='Calcula el hash SHA-256 de un texto.')
   parser.add_argument('texto', help='El texto a convertir en hash.')
   args = parser.parse_args()
 
   hash_resultante = calcular_sha256(args.texto)
+  print(f"String: {args.texto} \nHash Sha-256:")
   print(hash_resultante)
+  print(hash_resultante.upper())
+
+if __name__ == "__main__":
+    print_banner()
+    main()
+ 
