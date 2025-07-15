b76ad27528c5a349fea28da1732ae2b64a5adf29
Apuromafo <astronado@gmail.com>
2025-07-14 23:30:08 -0400

    Update Crea_malapi.py

--- 010_malapi_json/Crea_malapi.py
+++ 010_malapi_json/Crea_malapi.py
@@ -2,6 +2,9 @@ import requests
 import re
 import json
 from bs4 import BeautifulSoup
+author = 'Apuromafo'
+version = '0.0.1'
+date = '23.06.2024'
 
 def extraer_tablas(url):
     """Extrae las tablas que contienen <tbody> y no tienen atributos."""
